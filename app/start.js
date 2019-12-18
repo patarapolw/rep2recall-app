fd7cfc6da7ba72de124b9154e875c47e8158a528
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2019-12-18 07:18:45 +0700

    test build on linux

--- app/start.js
+++ app/start.js
@@ -5,4 +5,6 @@ if (process.platform === "win32") {
   open(`dist_electron/win-unpacked/${pkg.name}.exe`)
 } else if (process.platform === "darwin") {
   open(`dist_electron/mac/${pkg.name}.app`)
+} else  {
+  open(`dist_electron/linux-unpacked/${pkg.name}`)
 }

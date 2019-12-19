b751a8360d51a60caacd65cdbd810ee4bb3d5f51
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2019-12-20 01:11:10 +0700

    downgrade eslint to build for other platforms

--- app/release-win-linux.sh
+++ app/release-win-linux.sh
@@ -1,5 +1,3 @@
-echo ${PWD}
-
 docker run --rm -ti \
  --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
  --env ELECTRON_CACHE="/root/.cache/electron" \
--- app/start.js
+++ app/start.js
@@ -1,13 +1,14 @@
 const open = require("open")
 const pkg = require("./package.json")
 const yaml = require("js-yaml")
+const fs = require("fs")
 
-const config = yaml.safeLoad("./electron-builder.yml")
+const config = yaml.safeLoad(fs.readFileSync("./electron-builder.yml", "utf8"))
 
 if (process.platform === "win32") {
   open(`dist_electron/win-unpacked/${config.productName}.exe`)
 } else if (process.platform === "darwin") {
   open(`dist_electron/mac/${config.productName}.app`)
-} else  {
+} else {
   open(`dist_electron/linux-unpacked/${pkg.name}`)
 }
